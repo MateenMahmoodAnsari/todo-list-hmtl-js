04c64205f60e7e18d4a21df7025c516de438efd6
mateenMahmoodAnsari <mateenansari924@gmail.com>
2022-08-21 13:55:08 +0500

    fix: handled empty input field

--- hello.js
+++ hello.js
@@ -1,9 +1,9 @@
 const addToDo = () => {
 	const myInputEle = document.getElementById('myToDo');
 	const myNewToDoValue = myInputEle.value;
-	const toDoList = document.getElementById('toDoList');
-	const newListItem = document.createElement('li');
-	if (myinput.ele.value.lenght > 0) {
+	if (myNewToDoValue.length > 0) {
+		const toDoList = document.getElementById('toDoList');
+		const newListItem = document.createElement('li');
 		newListItem.innerHTML = myNewToDoValue;
 		toDoList.appendChild(newListItem);
 		myInputEle.value = '';
